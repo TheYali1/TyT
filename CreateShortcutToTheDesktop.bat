489aa045d135818e04221780373a438b8a73942d
TheYali1 <166911202+TheYali1@users.noreply.github.com>
2025-09-19 12:50:43 +0300

    Delete CreateShortcutToTheDesktop.bat

--- CreateShortcutToTheDesktop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "shortcut_path=%USERPROFILE%\Desktop\TyT.lnk"
-set "target_path=%~dp0main.bat"
-set "icon_path=%~dp0Icon.ico"
-set "shortcut_name=TyT"
-
-powershell -Command "$WshShell = New-Object -com WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%shortcut_path%'); $Shortcut.TargetPath = '%target_path%'; $Shortcut.IconLocation = '%icon_path%'; $Shortcut.WorkingDirectory = '%~dp0'; $Shortcut.Save()"
